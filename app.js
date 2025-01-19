ee317996e39e3db411477a203390121afb55de50
lxzcss <lucasilva589@gmail.com>
2025-01-19 15:54:12 -0300

    Atualização de funções e botões

--- app.js
+++ app.js
@@ -5,12 +5,20 @@
 
 
 
-//1. Criar uma array para armazenar os nomes dos amigos para o sorteio
+// Lista para armazenar os nomes dos participantes
 
 let nomesParaSorteio = [];
 
-let campoVazio = document.getElementById("amigo");
 
+// Função para exibir os nomes adicionados a lista
+function exibirNomeNaLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = (nomesParaSorteio);
+}
+
+for (let i = 0; i < nomesParaSorteio; i++) {
+    lista.innerHTML += `<li>${nomesParaSorteio}</li>`;
+}
 
 // Função para limpar o campo para que possa ser inserido outro nome no Jogo
 function limparCampo() {
@@ -18,28 +26,34 @@ function limparCampo() {
     nomeAmigo.value = "";
 }
 
+function limparLista() {
+    nomesParaSorteio = [];
+    let lista = document.getElementById("listaAmigos"); // Obtém o elemento <ul>
+    lista.innerHTML = ""; // Limpa o conteúdo da lista exibida
+    console.log("Limpou a lista");
+}
+
 
 // Função para adicionar o Nome inserido, em uma lista para que possa ser sorteado
 function adicionarAmigo() {
     // Obtém o valor do input
-    const input = document.getElementById("amigo").value.trim();
+    let input = document.getElementById("amigo").value;
 
     if (input === "") { // Verifica se o campo está vazio
         console.log("Ta funcionado, mas ta vazio");
         alert("Por favor, insira um nome para que seja sorteado!");
     } else {
         console.log("O botão adicionar foi clicado"); // Verifica se o botão está funcionando corretamente
-        nomesParaSorteio.push(input); // Adiciona o nome ao array
+        nomesParaSorteio.push(input); // Adiciona o nome à lista
         limparCampo(); // Limpa o campo de input
-        console.log(nomesParaSorteio); // Exibe o status do array
+        console.log(nomesParaSorteio); // Exibe os nomes da lista
+        exibirNomeNaLista();
     }
 }
 
 
 function sortearAmigo() {
-    console.log("O botão sortear foi clicado")
-}
-
-function digitarNome() {
-    let nomeAmigo = document.querySelector("input");
+    console.log("O botão sortear foi clicado");
+    limparLista();
+    console.log(nomesParaSorteio);
 }
